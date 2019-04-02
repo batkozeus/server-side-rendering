cc1b17821863af6704abda4117e7e9abdd767dde
Igor Protsyuk <batko_zeus@i.ua>
2019-04-02 19:24:00 +0300

    add basic routing

--- src/server/index.js
+++ src/server/index.js
@@ -2,8 +2,9 @@ import express from "express";
 import cors from "cors";
 import serialize from "serialize-javascript"
 import { renderToString } from "react-dom/server";
+import { matchPath } from "react-router-dom"
 import App from '../shared/App';
-import { fetchPopularRepos } from '../shared/api'
+import routes from '../shared/routes'
 
 const app = express();
 
@@ -15,8 +16,15 @@ app.use(cors());
 app.use(express.static("public"));
 
 app.get("*", (req, res, next) => {
-    fetchPopularRepos()
-    .then((data) => {
+    const activeRoute = routes.find(
+        (route) => matchPath(req.url, route)
+    ) || {}
+
+    const promise = activeRoute.fetchInitialData
+        ? activeRoute.fetchInitialData(req.path)
+        : Promise.resolve()
+
+    promise.then((data) => {
         const markup = renderToString(
             <App data={data} />
         );
@@ -29,13 +37,12 @@ app.get("*", (req, res, next) => {
                     <script src="/bundle.js" defer></script>
                     <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
                 </head>
-        
                 <body>
                     <div id="app">${markup}</div>
                 </body>
             </html>
         `)
-    })
+    }).catch(next);
 });
 
 app.listen(3000, () => {
